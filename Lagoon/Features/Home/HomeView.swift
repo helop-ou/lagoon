53448f40eb59c468cebda0b61150dd6b5ecf85e1
Jaagop Janson <dev@jaagop.eu>
2026-09-14 02:12:54 +0300

    feat: Home sits on the theme background (HEL-173)

--- Lagoon/Features/Home/HomeView.swift
+++ Lagoon/Features/Home/HomeView.swift
@@ -9,7 +9,7 @@ struct HomeView: View {
 
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            Theme.background.ignoresSafeArea()
 
             if viewModel.isLoading {
                 LoadingView()
